457c3e569fcee87d8c1821781fa91d3bbd37eef4
Sriram Panyam <sri.panyam@gmail.com>
2019-11-28 03:11:44 -0800

    Able to write out queries constructors for basic graphql syntaxes

--- modelzero/core/derivations.py
+++ modelzero/core/derivations.py
@@ -1,19 +1,59 @@
 
 from ipdb import set_trace
-import typing
+from typing import List, Union, Dict, Tuple
 from modelzero.core import types
+from taggedunion import Variant
+from taggedunion import Union as TUnion
+
+class Expr(TUnion):
+    fmap = Variant("FMap")
+    func = Variant("Func")
+    query = Variant("Query")
+    fpath = Variant("FieldPath")
+    bind = Variant("Bind")
+
+class Select(object):
+    """ Commands that projects a particular field into a source field. """
+    def __init__(self, *selectors : List[Union[str, Tuple[str, Expr]]]):
+        self.selectors = selectors
+
+class Include(object):
+    def __init__(self, query, **kwargs):
+        self.query = query
+        self.kwargs = kwargs
+
+class Command(TUnion):
+    select = Variant(Select)
+    include = Variant(Include)
 
 class FieldPath(object):
-    def __init__(self, value : typing.Union[str, typing.List[str]]):
+    def __init__(self, value : Union[str, List[str]]):
         if type(value) is str:
             value = [v.strip() for v in value.split("/") if v.strip()]
         self.parts = value
 
-class Derivation(object):
-    def __init__(self, fqn = None, **input_types : typing.Dict[str, types.Type]):
+class Func(object):
+    """ Commands that produce evaluated fields. """
+    def __init__(self, func_name : str, **func_kwargs : Dict[str, "Expr"]):
+        super().__init__()
+        self.func_name = func_name
+        self.func_kwargs = func_kwargs
+
+class FMap(object):
+    def __init__(self, expr : "Expr"):
+        self.expr = expr
+
+class Bind(object):
+    def __init__(self, func_expr : "Expr", **kwarg_exprs : Dict[str, "Expr"]):
+        self.func_expr = func_expr
+        self.kwarg_exprs = kwarg_exprs
+
+class Query(object):
+    def __init__(self, fqn = None, **input_types : Dict[str, types.Type]):
         self._inputs = {k: types.ensure_type(v) for k, v in input_types.items()}
-        self._derived_type = None
+        self._query_type = None
         self._name = self._fqn = None
+        self._commands : List[Union[Select, Include]] = []
         if fqn and fqn.strip():
             parts = [f.strip() for f in fqn.split(".") if f.strip()]
             self._fqn = ".".join(parts)
@@ -28,35 +68,42 @@ class Derivation(object):
     def get_input(self, name : str) -> types.Type:
         return self._inputs[name]
 
-    def include(self, source : str, field_path : FieldPath = None):
+    def apply(self, *commands : List["Command"]) -> "Command":
+        self._commands.extend(commands)
+        return self
+
+    def include(self, query : "Query", **kwargs : Dict[str, Expr]):
         """ Includes one or all fields from the source type at the root level
         of this derivation.
         """
-        pass
+        self._commands.append(Include(query, **kwargs))
+        return self
 
     def exclude(self, source_field_path : FieldPath):
         """ Removes particular field paths. """
         pass
 
-    def select(self, source_field_path : FieldPath,
-               target : str = None, *transformers):
-        """ Selects a particular source field as a field in the current root.
+    def select(self, *selectors : List[Select]):
+        """ Selects a particular source field as a field in the current root. """
+        self._commands.extend(selectors)
+        return self
 
-        This allows selection of a field in the source type (at an arbitrary depth) and placed at the top level of this derivation.  The target field is same as the source field name by default (and subject to error if duplicate).  The target field can be aliased to a different name optionally (say to avoid duplications).
-        """
-        pass
-
-    def __call__(self, *args, **kwargs):
+    def __call__(self, **kwargs):
         """ A derivation must also be callable since it is possible it is also used as a Transformer! """
-        pass
+        return Bind(self, **kwargs)
 
+    _counter = 1
     @property
-    def derived_type(self):
+    def query_type(self):
         """ Returns the type that corresponds the result of the derivations """
-        if self._derived_type is None:
-            self._derived_type = types.Type()
+        if self._query_type is None:
+            self._query_type = types.Type()
             classdict = dict(__fqn__ = self.fqn)
-            rec_class = types.RecordType.new_record_class(self.name, **classdict)
+            name = self.name
+            if not name:
+                name = f"Derivation_{self._counter}"
+                self._counter += 1
+            rec_class = types.RecordType.new_record_class(name, **classdict)
             rec_type = types.RecordType(rec_class)
-            self._derived_type.record_type = rec_type
-        return self._derived_type
+            self._query_type.record_type = rec_type
+        return self._query_type
--- tests/core/test_derivations.py
+++ tests/core/test_derivations.py
@@ -1,6 +1,6 @@
 
 from ipdb import set_trace
-from modelzero.core.derivations import Derivation
+from modelzero.core.derivations import Query, Func, Bind, FMap
 from modelzero.core.types import Type
 from modelzero.core.custom_types import MZTypes
 from modelzero.core.records import *
@@ -20,6 +20,20 @@ class User(Record):
     birthday = fields.Field(Type.as_record_type(Date))
     friends = fields.ListField("User")
 
+def test_derivation_init(mocker):
+    d = Query(user = User)
+    u = d.get_input("user")
+    assert u.is_record_type
+    assert u.record_class == User
+
+    dt = d.query_type
+    assert dt.is_record_type
+    rc = dt.record_class
+    assert rc
+
+    rm = rc.__record_metadata__
+    assert len(rm) == 0
+
 def test_basic(mocker):
     """
         {
@@ -37,16 +51,57 @@ def test_basic(mocker):
           }
         }
     """
-    d = Derivation("MyDerivation", user = User)
-    u = d.get_input("user")
-    assert u.is_record_type
-    assert u.record_class == User
+    d = Query(user = User).select(
+            "id",
+            "firstName",
+            "lastName",
+            ("birthday", Bind(Query(date = Date).select("month", "day"),
+                              date = "$user/birthday")),
+            ("friends", FMap(Query(friend = User).select("name")))
+        )
+    me = Query().select(
+            ("me", Bind(d, user = Func("get_current_user")))
+         )
 
-    dt = d.derived_type
-    rc = dt.record_class
-    assert dt.is_record_type
-    assert rc
+def test_example_10(mocker):
+    """
+        https://graphql.github.io/graphql-spec/draft/#example-34b2d
+        {
+            user(id: 4) {
+                id
+                name
+                profilePic(width: 100, height: 50)
+            }
+        }
+    """
+    d = Query(user = User).select(
+            "id",
+            "name",
+            ("profilePic", Func("get_profile_pic", id = "$user/id", width = 100, height = 50))
+        )
+    out = Query().select(
+            ("user", d(user = Func("get_user", id = 4)))
+         )
 
-    # Ensure it has exactly the fields needed
-    set_trace()
-    assert dt.fields
+
+def test_example_14(mocker):
+    """
+    https://graphql.github.io/graphql-spec/draft/#example-34435
+    {
+        user(id: 4) {
+            id
+            name
+            smallPic: profilePic(size: 64)
+            bigPic: profilePic(size: 1024)
+        }
+    }
+    """
+    d = Query(user = User).select(
+            "id",
+            "name",
+            ("smallPic", Func("get_profile_pic", id = "$user/id", size = 64)),
+            ("bigPic", Func("get_profile_pic", id = "$user/id", size = 1024))
+        )
+    out = Query().select(
+            ("user", d(user = Func("get_user", id = 4)))
+         )
